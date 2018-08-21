9ed3811086f35dd84cdedaf1c900bc6e150a9d72
Gaddi-Amaya-5IM9 <42345700+Gaddi-Amaya-5IM9@users.noreply.github.com>
2018-08-20 22:19:14 -0500

    Add files via upload

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,16 @@
+/*
+    Amaya Rivas Gaddi Salatiel
+ */
+package holamundo;
+
+/**
+ *
+ * @author Gaddi Amaya
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
